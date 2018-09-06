bfb6052d40e973b34cd7ff7a233dce4a45fca9db
Nkosi Phillip <internap627@gmail.com>
2018-09-06 02:46:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function takeANumber(line, name){
-  return `Welcome, ${name}. You are number ${line.length} in the line.`
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
